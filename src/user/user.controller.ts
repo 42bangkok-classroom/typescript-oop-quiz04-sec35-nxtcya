f028efa7d4926e52bb2c916f0d216f2c19d7102c
nxtcya <quinn3nxt@gmail.com>
2026-03-25 03:40:04 +0700

    sub02

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -3,11 +3,10 @@ import { UserService } from './user.service';
 
 @Controller('user')
 export class UserController {
-    constructor(private readonly userService : UserService){}
+  constructor(private readonly userService: UserService) {}
 
-    @Get('test')
-    gettest(){
-        return this.userService.test();
-    }
-
+  @Get('test')
+  gettest() {
+    return this.userService.test();
+  }
 }
